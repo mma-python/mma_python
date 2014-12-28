b048e4a820f0022e4b8361c7dee0294b79d1240c
mma-python <ircbot@SinisterPi.(none)>
2014-12-27 22:30:31 -0800

    Some refactoring

--- mma_events.py
+++ mma_events.py
@@ -1,29 +1,26 @@
 import willie
-import random
-import os
-from willie import module
 import requests
-import operator
 from willie.formatting import bold, underline
 
 @willie.module.commands('events')
 def events(bot, trigger):
-	results = build_fancy_dict_from_json(events_decoded)
-	bot.say(bold(underline("NEXT TEN UPCOMING EVENTS")))
+        events_decoded = return_events_list()
+        results = build_fancy_dict_from_json(events_decoded)
+        bot.say(bold(underline("NEXT TEN UPCOMING EVENTS")))
         for item in results:
                 bot.say("%s. %s" % (item, results[item]))
-        
+
 @willie.module.commands('event')
 def event(bot, trigger):
-	build_counter = 1
-	output_fight_card = return_card_info(trigger.group(2))
-	event_name = output_fight_card.split("-")[0].strip()
-	event_card = output_fight_card.split("-")[1]
-	output_fight_card_list = event_card.split(",")
-	bot.say(bold(event_name) + " " +  "has the following %s fights (A vs. B):" % len(output_fight_card_list))
-	for fight in output_fight_card_list:
-		bot.say(bold(str(build_counter)) + "." + " " + fight.strip())
-		build_counter = build_counter + 1
+        build_counter = 1
+        output_fight_card = return_card_info(trigger.group(2))
+        event_name = output_fight_card.split("-")[0].strip()
+        event_card = output_fight_card.split("-")[1]
+        output_fight_card_list = event_card.split(",")
+        bot.say(bold(event_name) + " " + "has the following %s fights (A vs. B):" % len(output_fight_card_list))
+        for fight in output_fight_card_list:
+                bot.say(bold(str(build_counter)) + "." + " " + fight.strip())
+                build_counter += 1
 
 
 def return_events_list():
@@ -31,27 +28,21 @@ def return_events_list():
         events_decoded = grab_mma_info.json()
         return events_decoded
 
+
 def build_fancy_dict_from_json(input_json):
+        events_decoded = return_events_list()
         event_catalog = {}
         build_counter = 1
         for x in input_json["mma_events"]:
-                event_number = build_counter
                 event_name = str(events_decoded["mma_events"][x]["event_name"])
                 event_time = str(events_decoded["mma_events"][x]["event_time"])
                 event_location = events_decoded["mma_events"][x]["event_location"]
                 event_catalog[events_decoded["mma_events"][x]["event_id"]] = "%s - %s - %s" % (event_name, event_location, event_time)
-                #sorted_x = sorted(event_catalog.items(), key=operator.itemgetter(0))
-                build_counter = build_counter + 1
+                build_counter += 1
         return event_catalog
 
-def main():
-        results = build_fancy_dict_from_json(events_decoded)
-        for item in results:
-                print "%s. %s" % (item, results[item])
-
-events_decoded = return_events_list()
 
 def return_card_info(event_id):
-        event = requests.get("http://127.0.0.1:5000/mma/event/%s" % event_id)
-        return event.text
+        event_request = requests.get("http://127.0.0.1:5000/mma/event/%s" % event_id)
+        return event_request.text
 
